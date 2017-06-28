8f5eec8bd8768d86fad4d86dcb62b11b99f85bd2
Jorge Alberto Nuñez Cuevas <jorgea.nunezcuevas@gmail.com>
2017-06-27 17:22:24 -0700

    recupera nodos de mascara

--- MascaraNodos.php
+++ MascaraNodos.php
@@ -1,19 +1,63 @@
 <?php
 
-$datos_json = '{"id_transaccion":0,"cuenta":"demo","user":"administrador","password":"Administr4dor","getPdf":true,"conceptos":[{"ClaveProdServ":"01010101","ClaveUnidad":"F52","Importe":"2250000","unidad":"TONELADA","noIdentificacion":"00001","cantidad":"1.5","descripcion":"ACERO","valorUnitario":"1500000","impuesto":"240000","porcentaje_imp":"16","Impuestos":{"Traslados":[{"Base":"2250000","Impuesto":"001","TipoFactor":"Tasa","TasaOCuota":"0.160000","Importe":"360000"}],"Retenciones":[{"Base":"2250000","Impuesto":"002","TipoFactor":"Tasa","TasaOCuota":"0.530000","Importe":"247500"}]},"CuentaPredial":{"Numero":"51888"}},{"ClaveProdServ":"required","ClaveUnidad":"required","Importe":"required","noIdentificacion":"766115","unidad":"Kg","cantidad":"50","descripcion":"Cebolla Blanca","valorUnitario":"10","impuesto":"0","porcentaje_imp":"0","Impuestos":{"Traslados":[{"Base":"2400","Impuesto":"001","TipoFactor":"Tasa","TasaOCuota":"0.160000","Importe":"384"}],"Retenciones":[{"Base":"2400","Impuesto":"002","TipoFactor":"Tasa","TasaOCuota":"0.530000","Importe":"264"}]},"InformacionAduanera":[{"NumeroPedimento":"15  48  4567  6001234"}]},{"ClaveProdServ":"01010101","ClaveUnidad":"F52","Importe":"2250000","unidad":"TONELADA","noIdentificacion":"00001","cantidad":"1.5","descripcion":"ACERO","valorUnitario":"1500000","impuesto":"240000","porcentaje_imp":"16","Impuestos":{"Trasladoss":[{"Base":"17000","Impuesto":"001","TipoFactor":"Tasa","TasaOCuota":"0.530000","Importe":"2720"}],"Retenciones":[{"Base":"17000","Impuesto":"002","TipoFactor":"Tasa","TasaOCuota":"0.160000","Importe":"1870"}]},"Parte":[{"ClaveProdServ":"01010101","NoIdentificacion":"055155","Cantidad":"1.0","Descripcion":"PARTE EJEMPLO","Unidad":"UNIDAD","ValorUnitario":"1.00","Importe":"1.00","InformacionAduanera":{"NumeroPedimento":"15  48  4567  6981235"}}]}],"datos_factura":{"Version":"3.3","FormaPago":"03","TipoCambio":"1.0","Confirmacion":"optional","Descuento":"0.00","MetodoPago":"PUE","RegimenFiscal":"601","LugarExpedicion":"22010","Moneda":"MXN","TipoDeComprobante":"0","NumCtaPago":"1245","CondicionesDePago":"Crédito","comentarios":"Probando generacion de CFDI","numero_de_pago":"1","cantidad_de_pagos":"1","ret_iva":"100","ret_isr":"200","no_sucural":"1","CfdiRelacionados":{"TipoRelacion":"01","CfdiRelacionado":{"UUID":"A39DA66B-52CA-49E3-879B-5C05185B0EF7"}},"Impuestos":{"TotalImpuestosRetenidos":"247500","TotalImpuestosTrasladados":"360000","Retenciones":[{"Impuesto":"002","Importe":"247500"}],"Traslados":[{"Impuesto":"001","TipoFactor":"Tasa","TasaOCuota":"0.160000","Importe":"360000"}]}},"method":"nueva_factura","cliente":{"id":"189","ResidenciaFiscal":"MEX","NumRegIdTrib":"0000000000000","UsoCFDI":"G01","nombre":"NUEVA EMPRESA DE EJEMPLO","rfc":"EEA9709083R7"}}';
-$masc_json = '{"_tipo":"obj","_elems":[{"conceptos":{"_tipo":"arr","_atribs":["ClaveProdServ","ClaveUnidad","impuesto","porcentaje_imp"],"_elems":[{"Impuestos":{"_tipo":"obj","_elems":[{"Traslados":{"_tipo":"arr","_atribs":["Base","Impuesto","TipoFactor","TasaOCuota","Importe"]}},{"Retenciones":{"_tipo":"arr","_atribs":["Base","Impuesto","TipoFactor","TasaOCuota","Importe"]}}]}},{"CuentaPredial":{"_tipo":"obj","_atribs":["Numero"]}},{"InformacionAduanera":{"_tipo":"arr","_atribs":["NumeroPedimento"]}},{"Parte":{"_tipo":"arr","_atribs":["ClaveProdServ","NoIdentificacion","Cantidad","Descripcion","Unidad","ValorUnitario","Importe"],"_elems":[{"InformacionAduanera":{"_tipo":"obj","_atribs":["NumeroPedimento"]}}]}}]}},{"datos_factura":{"_tipo":"obj","_atribs":["Confirmacion","NumCtaPago","comentarios","numero_de_pago","cantidad_de_pagos","ret_iva","ret_isr","no_sucural"],"_elems":[{"CfdiRelacionados":{"_tipo":"obj","_atribs":["TipoRelacion"],"_elems":[{"CfdiRelacionado":{"_tipo":"obj","_atribs":["UUID"]}}]}},{"Impuestos":{"_tipo":"obj","_elems":[{"Traslados":{"_tipo":"arr","_atribs":["TipoFactor"]}}]}}]}},{"cliente":{"_tipo":"obj","_atribs":["ResidenciaFiscal","NumRegIdTrib","UsoCFDI"]}}]}';
+$original = '{"id_transaccion":0,"cuenta":"demo","user":"administrador","password":"Administr4dor","getPdf":true,"conceptos":[{"ClaveProdServ":"01010101","ClaveUnidad":"F52","Importe":"2250000","unidad":"TONELADA","Impuestos":{"Traslados":[{"Base":"2250000","Impuesto":"001"}],"Retenciones":[{"Base":"2250000","Impuesto":"002"}]},"CuentaPredial":{"Numero":"51888"}},{"ClaveProdServ":"required","ClaveUnidad":"required","Importe":"required","noIdentificacion":"766115","unidad":"Kg","Impuestos":{"Traslados":[{"Base":"2400","Impuesto":"001"}],"Retenciones":[{"Base":"2400","Impuesto":"002"}]},"InformacionAduanera":[{"NumeroPedimento":"15  48  4567  6001234"}]},{"ClaveProdServ":"01010101","ClaveUnidad":"F52","Importe":"2250000","unidad":"TONELADA","Impuestos":{"Traslados":[{"Base":"17000","Impuesto":"001"}],"Retenciones":[{"Base":"17000","Impuesto":"002"}]},"Parte":[{"ClaveProdServ":"01010101","NoIdentificacion":"055155","Cantidad":"1.0","Descripcion":"PARTE EJEMPLO","InformacionAduanera":{"NumeroPedimento":"15  48  4567  6981235"}}]}],"datos_factura":{"Version":"3.3","FormaPago":"03","Confirmacion":"optional","NumCtaPago":"1245","CfdiRelacionados":{"TipoRelacion":"01","CfdiRelacionado":{"UUID":"A39DA66B-52CA-49E3-879B-5C05185B0EF7"}},"Impuestos":{"TotalImpuestosRetenidos":"247500","TotalImpuestosTrasladados":"360000","Retenciones":[{"Impuesto":"002","Importe":"247500"}],"Traslados":[{"Impuesto":"001","TipoFactor":"Tasa"}]}},"method":"nueva_factura","cliente":{"id":"189","ResidenciaFiscal":"MEX","UsoCFDI":"G01","rfc":"EEA9709083R7"}}';
+$masc_json = '{"_tipo":"obj","_elems":[{"conceptos":{"_tipo":"arr","_atribs":["ClaveProdServ","ClaveUnidad"],"_elems":[{"Impuestos":{"_tipo":"obj","_elems":[{"Traslados":{"_tipo":"arr","_atribs":["Base","Impuesto"]}},{"Retenciones":{"_tipo":"arr","_atribs":["Base","Impuesto"]}}]}},{"CuentaPredial":{"_tipo":"obj","_atribs":["Numero"]}},{"InformacionAduanera":{"_tipo":"arr","_atribs":["NumeroPedimento"]}},{"Parte":{"_tipo":"arr","_atribs":["ClaveProdServ","NoIdentificacion"],"_elems":[{"InformacionAduanera":{"_tipo":"obj","_atribs":["NumeroPedimento"]}}]}}]}},{"datos_factura":{"_tipo":"obj","_atribs":["Confirmacion","NumCtaPago"],"_elems":[{"CfdiRelacionados":{"_tipo":"obj","_atribs":["TipoRelacion"],"_elems":[{"CfdiRelacionado":{"_tipo":"obj","_atribs":["UUID"]}}]}},{"Impuestos":{"_tipo":"obj","_elems":[{"Traslados":{"_tipo":"arr","_atribs":["TipoFactor"]}}]}}]}},{"cliente":{"_tipo":"obj","_atribs":["ResidenciaFiscal","UsoCFDI"]}}]}';
+file_put_contents('original.json', $original);
 
-$datos = json_decode($datos_json);
+$datos = json_decode($original);
 $masc = json_decode($masc_json);
 
-$datos_extra = extraerMascara($datos, $masc);
-file_put_contents('datos_extra.json', json_encode($datos_extra));
+$nodos_masc = extraerMascara($datos, $masc);
+file_put_contents('nodos_masc.json', json_encode($nodos_masc));
 
+$parcial_json = '{"id_transaccion":0,"cuenta":"demo","user":"administrador","password":"Administr4dor","getPdf":true,"conceptos":[{"Importe":"2250000","unidad":"TONELADA"},{"Importe":"required","noIdentificacion":"766115","unidad":"Kg"},{"Importe":"2250000","unidad":"TONELADA","Parte":[{"Cantidad":"1.0","Descripcion":"PARTE EJEMPLO"}]}],"datos_factura":{"Version":"3.3","FormaPago":"03","Impuestos":{"TotalImpuestosRetenidos":"247500","TotalImpuestosTrasladados":"360000","Retenciones":[{"Impuesto":"002","Importe":"247500"}],"Traslados":[{"Impuesto":"001"}]}},"method":"nueva_factura","cliente":{"id":"189","rfc":"EEA9709083R7"}}';
+$parcial = json_decode($parcial_json);
+file_put_contents('parcial.json', $parcial_json);
+
+$completo = combinarNodos($parcial, $nodos_masc);
+file_put_contents('completo.json', json_encode($completo));
+
+$completo_dom = new DomDocument('1.0', 'UTF-8');
 $prefijo = 'cfdi:';
-$doc_dom = new DomDocument('1.0', 'UTF-8');
+xml2dom($completo_dom, $completo_dom, $prefijo, 'Comprobante', $completo);
+file_put_contents('completo.xml', $completo_dom->saveXML());
 
-xml2dom($doc_dom, $doc_dom, $prefijo, 'Comprobante', $datos_extra);
-echo $doc_dom->saveXML();
+function combinarNodos($parcial, $nodos_masc){
+    if(is_array($nodos_masc))
+    {
+        $arr = [];
+        
+        foreach($nodos_masc as $llave => $valor)
+        {
+            array_push($arr, combinarNodos($parcial[$llave], $valor));
+        }
+        
+        return $arr;
+    }
+    else if(is_object($nodos_masc))
+    {
+        $completo = new stdClass();
+        
+        foreach($parcial as $llave => $valor)
+        {
+            if(!isset($nodos_masc->$llave)){
+                $completo->$llave = $valor;
+            }
+        }
+        
+        foreach($nodos_masc as $llave => $valor)
+        {
+            if(isset($parcial->$llave)){
+                $completo->$llave = combinarNodos($parcial->$llave, $valor);
+            }
+            else {
+                $completo->$llave = $valor;
+            }
+        }
+        
+        return $completo;
+    }
+}
 
 function xml2dom($doc_dom, $elem_dom, $prefijo, $campo, $dato)
 {
